889e311418ee69a38c1c3b8ca20de0602f963e95
Lukanin Mikhail <mlukanin@ics.perm.ru>
2019-10-02 15:54:37 +0500

    Первая версия сайта ТОС

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+    $(".dropdown-trigger").dropdown();
+    (function(){
+        var date = new Date();
+        let days = date.getDay().toString().length == 1 ? '0'+date.getDay():  date.getDay(), 
+        months = date.getMonth().toString().length == 1 ? '0'+date.getMonth():  date.getMonth(),
+        seconds = date.getSeconds().toString().length == 1 ? '0'+date.getSeconds():  date.getSeconds(),
+        hours = date.getHours().toString().length == 1 ? '0'+date.getHours():  date.getHours(),
+        minutes = date.getMinutes().toString().length == 1 ? '0'+date.getMinutes():  date.getMinutes();
+        var time = 'Дата: '+ days+':'+ months+':'+ date.getFullYear()+' Время: ' +hours+':'+minutes+':'+seconds;
+        $('#actuallyTime').text(time)
+        window.setTimeout(arguments.callee, 1000);
+    })();
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
